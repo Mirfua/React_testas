7087f5ebae4dcd7bc2660ab8fa1878105a7b2458
Jevgenijus <j.suchodolskis@gmail.com>
2021-10-06 14:01:09 +0300

    test

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ console.log(mas1)
 //2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
 // iteracijoje atspausdinti indeksą ir jo reikšmę;
 const mas2 = [13, '785', 92, 'labas', 522, 6984];
-
-
+for(let i = 0; i < mas2.length; i++) {
+    console.log(i, mas2[i]);
+}
 
 
  
